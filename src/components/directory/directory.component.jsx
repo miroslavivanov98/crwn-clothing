2956097924f30d036546a16af9c267902d703e8a
miroslavivanov98 <32927661+miroslavivanov98@users.noreply.github.com>
2021-10-12 18:12:39 +0300

    implemented firebase utils, included ability to store auth users into firestorm db

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,59 +1,58 @@
-import React,{Component} from 'react'
-import MenuItem from '../menu-item/menu-item.component'
+import React, { Component } from 'react';
+import MenuItem from '../menu-item/menu-item.component';
 
-import './directory.styles.scss'
+import './directory.styles.scss';
 
+class Directory extends React.Component {
+  constructor() {
+    super();
 
-class Directory extends React.Component{
-    constructor(){
-        super();
-
-        this.state = {
-            sections: [{
-                title: 'Hats',
-                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                id: 1,
-                linkUrl: 'hats'
-            },
-            {
-                title: 'Jackets',
-                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                id: 2,
-                linkUrl: ''
-            },
-            {
-                title: 'Sneakers',
-                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                id: 4,
-                linkUrl: ''
-            },
-            {
-                title: 'Womens',
-                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                id: 4,
-                size: 'large',
-                linkUrl: ''
-            },
-            {
-                title: 'Mens',
-                imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                id: 5,
-                size: 'large',
-                linkUrl: ''
-            }
-            ]
-        }
-    }
-    render(){
-        return(
-            <div className="directory-menu">
-                {this.state.sections.map(({id, ...otherSectionProps}) => 
-                (<MenuItem key = {id} {...otherSectionProps}/>
-                    ))
-                }
-            </div>
-        )
-    }
+    this.state = {
+      sections: [
+        {
+          title: 'Hats',
+          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          id: 1,
+          linkUrl: 'hats',
+        },
+        {
+          title: 'Jackets',
+          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          id: 2,
+          linkUrl: 'jackets',
+        },
+        {
+          title: 'Sneakers',
+          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          id: 4,
+          linkUrl: 'sneakers',
+        },
+        {
+          title: 'Womens',
+          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          id: 4,
+          size: 'large',
+          linkUrl: 'womens',
+        },
+        {
+          title: 'Mens',
+          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          id: 5,
+          size: 'large',
+          linkUrl: 'mens',
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Directory;
